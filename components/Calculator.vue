6d42592841a935bb74d1a6934293a410fc162933
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-27 09:13:21 +1100

    initial commit

---
+++ components/Calculator.vue
@@ -0,0 +1,14 @@
+<template>
+  <GenshinSlider leftLabel="Stability" />
+  <GenshinSlider leftLabel="Internal Conflict" />
+  <GenshinSlider leftLabel="Sacrifice" />
+  <GenshinSlider leftLabel="Energy" />
+  <GenshinSlider leftLabel="Loss" />
+  <GenshinSlider leftLabel="Ability/Skill" />
+  <GenshinSlider leftLabel="Introvertedness" />
+  <GenshinSlider leftLabel="Extrovertedness" />
+  <GenshinSlider leftLabel="Self-driven" />
+  <GenshinSlider leftLabel="Mind" />
+  <GenshinSlider leftLabel="Heart" />
+  <GenshinSlider leftLabel="Protectiveness" />
+</template>
---
+++ components/GenshinSlider.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    title: string,
+    leftLabel: string,
+    rightLabel: string
+  }>()
+</script>
+
+<style module>
+  .sliderParent {
+    display: flex;
+    gap: 16px;
+    margin: 1em;
+    background-color: #f1e9ac;
+    padding: 1em 2em;
+    border-radius: 999px;
+  }
+
+  .sliderParent > span {
+    width: 100%;
+  }
+
+  .sliderParent > input {
+    width: 100%;
+  }
+</style>
+
+<template>
+  <div :class="$style.sliderParent">
+    <span>{{ leftLabel }}</span>
+    <input type="range" min="0" max="5" step="0.01">
+  </div>
+</template>
