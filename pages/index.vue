d23a200a988322150f3e74e0f5eeeaaaee868389
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-27 09:33:52 +1100

    okay lets do this in nextjs

--- pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1>Genshin Vision Calculator</h1>
-  <p>It's like a personality test, but really poorly documented. Such is the complexity of words, that a single label can represent thousands of concepts... It's up to you to decide which ones are most important for this purpose.</p>
-  <Calculator />
-</template>
