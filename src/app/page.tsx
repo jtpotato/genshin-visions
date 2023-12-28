0a2818801e04c1cc14825f826ad8aecf894fbc71
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-28 17:53:13 +1100

    update images.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import Calculator from "./Calculator";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
-    <main className="p-4">
-      <h1>Genshin Vision Calculator</h1>
-      <p className="text-genshin-text">by jtpotato</p>
-      <br />
-      <p className="max-w-[40rem] text-genshin-text">It's like a personality test but with really poorly written documentation. It's up to you to figure out what each number is for :)</p>
-      <Calculator />
-    </main>
+    <div className={styles.pageParent}>
+      <main className="p-4 text-white md:p-8 md:pt-16">
+        <h1>Genshin Vision Calculator</h1>
+        <p className="text-white">by jtpotato</p>
+        <br />
+        <p className="max-w-[40rem] text-neutral-400">It's like a personality test but with really poorly written documentation. It's up to you to figure out what each number is for :)</p>
+        <Calculator />
+      </main>
+      <div className={styles.pageBackground} style={{ backgroundImage: "url('/images/Liyue.webp')" }}>
+
+      </div>
+    </div>
+
   )
 }
